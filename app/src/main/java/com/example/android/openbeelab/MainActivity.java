3b93589696b48b6915eae91ad1a6dbfdc5e55177
Elorri <etchemendy.elorri@gmail.com>
2015-11-24 19:38:17 +0100

    Idem without AsyncTask

--- app/src/main/java/com/example/android/openbeelab/MainActivity.java
+++ app/src/main/java/com/example/android/openbeelab/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.android.openbeelab;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import java.io.IOException;
 import java.util.List;
 
 import retrofit.Call;
+import retrofit.Callback;
 import retrofit.GsonConverterFactory;
+import retrofit.Response;
 import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,51 +19,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BackgroundTask movieTask = new BackgroundTask();
-        movieTask.execute();
+
+        givesContributors();
 
     }
 
 
-    public class BackgroundTask extends AsyncTask<Void, Void, Void> {
+    private Void givesContributors() {
+        // Create a very simple REST adapter which points the GitHub API.
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(NetworkService.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            return givesContributors();
-        }
+        // Create an instance of our GitHub API interface.
+        NetworkService.GitHub github = retrofit.create(NetworkService.GitHub.class);
 
-        @Override
-        protected void onPostExecute(Void result) {
-            if (result != null) {
+        // Create a call instance for looking up Retrofit contributors.
+        Call<List<NetworkService.Contributor>> call = github.contributors("square", "retrofit");
 
+        call.enqueue(new Callback<List<NetworkService.Contributor>>() {
+            @Override
+            public void onResponse(Response<List<NetworkService.Contributor>> response, Retrofit retrofit) {
+                List<NetworkService.Contributor> contributors = response.body();
+                for (NetworkService.Contributor contributor : contributors) {
+                    Log.e("qqc", contributor.login + " (" + contributor.contributions + ")");
+                }
             }
-        }
 
-
-    }
-
-    private Void givesContributors(){
-        try {
-            // Create a very simple REST adapter which points the GitHub API.
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(NetworkService.API_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-
-            // Create an instance of our GitHub API interface.
-            NetworkService.GitHub github = retrofit.create(NetworkService.GitHub.class);
-
-            // Create a call instance for looking up Retrofit contributors.
-            Call<List<NetworkService.Contributor>> call = github.contributors("square", "retrofit");
-
-            // Fetch and print a list of the contributors to the library.
-            List<NetworkService.Contributor> contributors = call.execute().body();
-            for (NetworkService.Contributor contributor : contributors) {
-                Log.e("qqc",contributor.login + " (" + contributor.contributions + ")");
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e("qqc", "onFailure");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
+
         return null;
     }
 
