406c8aac416776097dc4aafe9eeb99b75a3249ff
Elorri <etchemendy.elorri@gmail.com>
2015-12-01 20:41:37 +0100

    Add BeeSyncService

---
+++ app/src/main/java/com/example/android/openbeelab/sync/BeeService.java
@@ -0,0 +1,26 @@
+package com.example.android.openbeelab.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+/**
+ * Created by Elorri on 01/12/2015.
+ */
+public class BeeService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static BeeSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new BeeSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
