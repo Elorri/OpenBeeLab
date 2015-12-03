62cc8d8bec6dd675adab3dadbc3f7f1b32395be8
Elorri <etchemendy.elorri@gmail.com>
2015-12-03 10:45:06 +0100

    Add userDB choice in settings

--- app/src/main/java/com/example/android/openbeelab/MainActivity.java
+++ app/src/main/java/com/example/android/openbeelab/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.openbeelab;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.android.openbeelab.db.BeeContract;
@@ -85,6 +88,28 @@ public class MainActivity extends AppCompatActivity {
         public void onValueDeselected() {
             // TODO Auto-generated method stub
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
